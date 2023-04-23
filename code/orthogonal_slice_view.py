41b23e8d6f1aa342ca545bae73c44685cca1ba09
Bardh R <bardhrushiti@gmail.com>
2023-04-23 00:50:03 -0400

    Aligning planes with axis.

--- code/orthogonal_slice_view.py
+++ code/orthogonal_slice_view.py
@@ -3,7 +3,8 @@ import numpy as np
 from utils.Slicer import Slicer
 
 # create an example numpy volume
-volume = np.load("numpy_arrays_scratch/scan.npy")
+volume = np.load("ndarrays/scan.npy")
+volume.transpose((2, 1, 0))
 
 # convert the volume to a PyVista data object
 mesh = pv.wrap(volume)
@@ -15,10 +16,10 @@ pl.subplot(0, 0)
 # pl.enable_parallel_projection()
 pl.add_slider_widget(slicer.slice_z, mesh.bounds[0:2], )
 
-pl.subplot(1, 0)
+pl.subplot(0, 1)
 pl.add_slider_widget(slicer.slice_y, mesh.bounds[4:6], )
 
-pl.subplot(0, 1)
+pl.subplot(1, 0)
 pl.add_slider_widget(slicer.slice_x, mesh.bounds[2:4], )
 
 pl.show()
