9f7294be13b50c2b93a9be5a269e1f21251eba7a
Bardh R <bardhrushiti@gmail.com>
2023-05-04 16:35:59 -0400

    Plotting in 3|1|3 for orthogonal slices

--- code/utils/Slicer.py
+++ code/utils/Slicer.py
@@ -6,8 +6,8 @@ class Slicer:
     def slice_x(self, location):
         slc = self.mesh.slice(normal=(0, 0, 1), origin=(self.mesh.center[0], self.mesh.center[1], location))
 
-        self.plotter.subplot(2, 0)
-        self.plotter.add_mesh(slc, name='slice_x')
+        self.plotter.subplot(6)
+        self.plotter.add_mesh(slc, name='slice_x', pickable=False)
         self.plotter.camera_position = 'xy'
         self.plotter.camera.roll = 90
         self.plotter.add_text("X-Axis", font_size=14, position="lower_left")
@@ -20,8 +20,8 @@ class Slicer:
     def slice_y(self, location):
         slc = self.mesh.slice(normal=(0, 1, 0), origin=(self.mesh.center[0], location, self.mesh.center[2]))
 
-        self.plotter.subplot(1, 0)
-        self.plotter.add_mesh(slc, name='slice_y')
+        self.plotter.subplot(5)
+        self.plotter.add_mesh(slc, name='slice_y', pickable=False)
         self.plotter.camera_position = 'xz'
         self.plotter.camera.roll = 90
         self.plotter.add_text("Y-Axis", font_size=14, position="lower_left")
@@ -32,8 +32,8 @@ class Slicer:
     def slice_z(self, location):
         slc = self.mesh.slice(normal=(1, 0, 0), origin=(location, *self.mesh.center[1:]))
 
-        self.plotter.subplot(0, 0)
-        self.plotter.add_mesh(slc, name='slice_z')
+        self.plotter.subplot(4)
+        self.plotter.add_mesh(slc, name='slice_z', pickable=False)
         self.plotter.camera_position = 'yz'
         self.plotter.camera.roll = 0
         self.plotter.add_text("Z-Axis", font_size=14, position="lower_left")
